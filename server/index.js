8a7d82610987e7d8ae3bb75741cbb997df1c6270
Lachlan <lockodaniels@gmail.com>
2025-09-24 17:34:10 +1000

    Refactor SPA routing to explicitly define routes for serving index.html

--- server/index.js
+++ server/index.js
@@ -185,10 +185,17 @@ app.get('/myfavourites', checkAuth, async (req, res) => {
 });
 
 // Serve SPA
+// Serve SPA static files
 app.use(express.static(path.join(__dirname, "client")));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "index.html"));
+
+// Explicit SPA routes
+const spaRoutes = ['/', '/myfavourites'];
+spaRoutes.forEach(route => {
+    app.get(route, (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "index.html"));
+    });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
