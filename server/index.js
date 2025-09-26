5ab649810b634a09b582e8cb8e54346da3994550
Riley Jackson-Kearnes <riley.jk.au@gmail.com>
2025-09-26 21:14:22 +1000

    made cookies unsecure

--- server/index.js
+++ server/index.js
@@ -49,11 +49,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'none',
-  httpOnly: true,
-  maxAge: 24 * 60 * 60 * 1000
-}
+    secure: false,
+    sameSite: 'none',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
 }));
 
 const checkAuth = (req, res, next) => {
