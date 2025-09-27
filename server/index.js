b74a3a53ae5c9a1c8cc5a5e33df5fbe9e8ae0de1
Riley Jackson-Kearnes <riley.jk.au@gmail.com>
2025-09-27 10:28:54 +1000

    made cookies secure again

--- server/index.js
+++ server/index.js
@@ -49,8 +49,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000
   }
